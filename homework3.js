b739eb4bc8bedce2871b3df7eb49c5284b4b6c24
AndreyKarapetyan <andrey.karapetyan2000@mail.ru>
2017-09-22 10:50:45 +0400

    Homework3

---
+++ homework3.js
@@ -0,0 +1,46 @@
+const niggerfunc=function(num1, num2, num3)
+{
+	const mult=num1*num2*num3;
+	return mult;
+};
+const madfunc=function()
+{
+	return 10;
+};
+const jewfunc=function(money1, money2)
+{
+	const income=money1+money2;
+	return;
+};
+const fullName=function(firstName, lastName)
+{
+	const passportname=firstName+" "+lastName;
+	return passportname;
+};
+const whyyyfunc = function(name1, name2, name3)
+{
+	if(name1.length>name2.length && name1.length>name3.length)
+		return name1.length;
+	else if(name2.length>name1.length && name2.length>name3.length)
+		return name2.length;
+	else
+		return name3.length;
+};
+const cleverfunc=function(num1, num2)
+{
+	if(num1===num2)
+		return 0;
+	else if(num1>num2)
+		return 1;
+	else 
+		return -1;
+};
+const courtfunc=function(value1, value2, value3)
+{
+	if(!!value1===true)
+		return value1;
+	else if(!!value2===true)
+		return value2;
+	else if(!!value3===true)
+		return value3;
+};
